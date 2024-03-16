8e74ed92202b677629da72cd13fdf0cc99e5a109
Pjotr van der Horst <pvanderhorst@swis.nl>
2024-03-16 16:41:18 +0100

    removed old code comment

--- src/PromptsThemesServiceProvider.php
+++ src/PromptsThemesServiceProvider.php
@@ -26,32 +26,4 @@ class PromptsThemesServiceProvider extends PackageServiceProvider
             Prompt::theme('custom');
         }
     }
-
-//    public function boot(): void
-//    {
-//        $this->registerPublishables();
-//
-//        if (config('prompts-themes.spinner') !== SpinnerRendererDefault::class) {
-//            Prompt::addTheme('custom', [
-//            Spinner::class => config('prompts-themes.spinner'),
-//            ]);
-//            Prompt::theme('custom');
-//        }
-//    }
-//
-//    public function register(): void
-//    {
-//        $this->mergeConfigFrom(__DIR__.'/../config/prompts-themes.php', 'prompts-themes');
-//    }
-//
-//    protected function registerPublishables(): void
-//    {
-//        if (!$this->app->runningInConsole()) {
-//            return;
-//        }
-//
-//        $this->publishes([
-//        __DIR__.'/../config/prompts-themes.php' => config_path('prompts-themes.php'),
-//        ], 'config');
-//    }
 }
